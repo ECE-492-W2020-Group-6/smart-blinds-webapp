5c41010307eba33f88cbb3e323705ed3c6043386
Kevin de Haan <kbehaan@gmail.com>
2020-02-02 16:55:07 -0700

    Updates

---
+++ precache-manifest.0c554fa54ca192cd2ec8451d861d98e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8912693e3d59ef963772ba42f83e41e",
+    "url": "/smart-blinds-webapp/index.html"
+  },
+  {
+    "revision": "5e35c86b451663cb8db2",
+    "url": "/smart-blinds-webapp/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "80e7a4ea616389f530b7",
+    "url": "/smart-blinds-webapp/static/js/2.d27a6eab.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/smart-blinds-webapp/static/js/2.d27a6eab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e35c86b451663cb8db2",
+    "url": "/smart-blinds-webapp/static/js/main.4c0668f8.chunk.js"
+  },
+  {
+    "revision": "3863d32b3d2f57f23396",
+    "url": "/smart-blinds-webapp/static/js/runtime-main.4cb32e1f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.af791db3034b34dd7f2972683a36c132.js"
+  "/smart-blinds-webapp/precache-manifest.0c554fa54ca192cd2ec8451d861d98e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/smart-blinds-webapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
