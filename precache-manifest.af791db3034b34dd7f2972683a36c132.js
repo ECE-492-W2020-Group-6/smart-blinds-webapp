8e0a89cfbb191f5ead160367f30f6aa799a019e3
Kevin de Haan <kbehaan@gmail.com>
2020-02-02 16:52:23 -0700

    Updates

---
+++ precache-manifest.af791db3034b34dd7f2972683a36c132.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2dae5e4fcac76b4c55ea1da6a7bda32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "776b2d0d27a4a257e7bf",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "80e7a4ea616389f530b7",
+    "url": "/static/js/2.d27a6eab.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.d27a6eab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "776b2d0d27a4a257e7bf",
+    "url": "/static/js/main.ca83725a.chunk.js"
+  },
+  {
+    "revision": "6956b6a0646d3f5b8598",
+    "url": "/static/js/runtime-main.6b993f51.js"
+  }
+]);
