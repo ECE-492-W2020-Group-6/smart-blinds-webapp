1d4ea005d724e5179d2754ecbcefe83b1502acaf
kdehaan <kdehaan@users.noreply.github.com>
2020-03-06 21:13:52 +0000

    deploy: ee1cdb28616dfad92b12c2318408c2ac2289db24

--- precache-manifest.540ad6de2121c90ea7ee32fc30cb63b3.js
+++ precache-manifest.540ad6de2121c90ea7ee32fc30cb63b3.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c9d148afb8f95ca3566f3f6c459b7911",
+    "revision": "a6b9687cd48fb495c528342349fe5ca2",
     "url": "/smart-blinds-webapp/index.html"
   },
   {
-    "revision": "9c32948bcc68013ae13e",
+    "revision": "cbf24a838c5f7bc9c207",
     "url": "/smart-blinds-webapp/static/css/main.34de6062.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/smart-blinds-webapp/static/js/2.6dcbea9a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9c32948bcc68013ae13e",
-    "url": "/smart-blinds-webapp/static/js/main.7201e8e4.chunk.js"
+    "revision": "cbf24a838c5f7bc9c207",
+    "url": "/smart-blinds-webapp/static/js/main.f5ff6e26.chunk.js"
   },
   {
     "revision": "3863d32b3d2f57f23396",
